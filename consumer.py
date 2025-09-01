f2b34175e70918d818f49a890f5d859e5ec65a11
Suraj Gupta <surajgupta689800@gmail.com>
2025-09-01 18:49:04 +0530

    send email otp done

--- consumer.py
+++ consumer.py
@@ -1,8 +1,57 @@
 import pika
+import numpy as np
+import smtplib
+from email.message import EmailMessage
+import os, json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FROM_EMAIL = os.getenv("FROM_EMAIL")
+EMAIL_PWD = os.getenv("EMAIL_PWD")
+
+print(FROM_EMAIL, EMAIL_PWD)
+def generate_otp():
+    return np.random.randint(1000, 9999)
+
+def send_otp(user_name, to_email):
+    otp = generate_otp()
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(FROM_EMAIL, EMAIL_PWD)
+
+            msg = EmailMessage()
+            msg["Subject"] = "OTP Verification"
+            msg["From"] = FROM_EMAIL
+            msg["To"] = to_email
+
+            msg.set_content(
+                f"Hello {user_name},\n\n"
+                f"We received a request to verify your email: {to_email}.\n"
+                f"Your One-Time Password (OTP) is: {otp}\n\n"
+                "Please enter this code to continue.\n\n"
+                "Best regards,\n"
+                "Suraj Gupta"
+            )
+
+            server.send_message(msg)
+            print("OTP Sent Succefully...")
+        return otp
+    except Exception as e:
+        import traceback
+        print(f"Something went wrong to send otp! {str(e)}")
+        traceback.print_exc()
+        return None
 
 def callback(ch, method, properties, body):
     message = body.decode()
-    print(message)
+    message = json.loads(message)
+    user_name = message.get("user_name")
+    email = message.get("email")
+
+    otp = send_otp(user_name, email)
+    print(otp)
 
 params = pika.URLParameters("amqps://bvbfmndz:mg0thl9d08gagtu2oQSwIU7JDRXQjsPL@rabbit.lmq.cloudamqp.com/bvbfmndz")
 connection = pika.BlockingConnection(params)
--- rbmqapp/views.py
+++ rbmqapp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .rabbitmq import publish_message
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 
+@csrf_exempt
 def index(request):
-    publish_message("Hi this is a message from rbmqapp")
-    return HttpResponse("massege pushed into RabbitMQ")
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        message = json.dumps(data)
+        publish_message(message)
+        return JsonResponse({"msg": "message pushed into RabbitMQ"})
+    
+    return JsonResponse({"msg": "wrong method"})
